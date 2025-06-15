23847ec9c98a984a052d738ee0a3c595d1dd7d32
Patrik Andersson <patrik@priva-innovation.eu>
2025-06-15 16:48:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function setup() {
-    createCanvas(400, 400);
+    let cnv = createCanvas(400, 400);
+    cnv.parent("canvas-container"); // Placera canvasen i div-container
 }
-function draw() {
+
 a = (x, y, d = mag(k = (4 + sin(y * 2 - t) * 3) * cos(x / 29), e = y / 8 - 13)) => 
     point(
         (q = 3 * sin(k * 2) + .3 / k + sin(y / 25) * k * (9 + 4 * sin(e * 9 - d * 3 + t * 2))) + 30 * cos(c = d - t) + 200,
@@ -9,5 +10,13 @@ a = (x, y, d = mag(k = (4 + sin(y * 2 - t) * 3) * cos(x / 29), e = y / 8 - 13)) 
     );
 
 t = 0;
+
+function draw() {
+    background(0); // Svart bakgrund
+    stroke(255); // Vita linjer och punkter
+    strokeWeight(2); // Gör punkterna tjockare
+
+    for (t += PI / 240, i = 1e4; i--; ) {
+        a(i, i / 235);
     }
 }
