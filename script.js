7a0d2a2a78aa0aeaf1720ad678d652a8da1cce08
Patrik Andersson <patrik@priva-innovation.eu>
2025-06-15 16:41:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function setup() {
     createCanvas(400, 400);
 }
-
+function draw() {
 a = (x, y, d = mag(k = (4 + sin(y * 2 - t) * 3) * cos(x / 29), e = y / 8 - 13)) => 
     point(
         (q = 3 * sin(k * 2) + .3 / k + sin(y / 25) * k * (9 + 4 * sin(e * 9 - d * 3 + t * 2))) + 30 * cos(c = d - t) + 200,
@@ -9,7 +9,8 @@ a = (x, y, d = mag(k = (4 + sin(y * 2 - t) * 3) * cos(x / 29), e = y / 8 - 13)) 
     );
 
 t = 0;
-
+    }
+}
 function draw() {
     t || createCanvas(w = 400, w);
     background(0); // Svart bakgrund
@@ -19,18 +20,8 @@ function draw() {
         a(i, i / 235);
     }
 }
-function draw() {
-    t || createCanvas(w = 400, w);
-    background(0); // Svart bakgrund
-    stroke(255); // Helt vit färg
-    strokeWeight(2); // Tydligare linjer och punkter
-
-    for (t += PI / 240, i = 1e4; i--; ) {
-        a(i, i / 235);
-    }
 }
 function draw() {
-    background(0); // Svart bakgrund
     stroke(255); // Vit färg
     strokeWeight(4); 
     ellipse(width / 2, height / 2, 50, 50); // Testcirkel
