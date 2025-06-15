5f6be5dd20cc6994d5d3c45abee6cbe0a11a2472
Patrik Andersson <patrik@priva-innovation.eu>
2025-06-15 16:43:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,14 +11,3 @@ a = (x, y, d = mag(k = (4 + sin(y * 2 - t) * 3) * cos(x / 29), e = y / 8 - 13)) 
 t = 0;
     }
 }
-function draw() {
-    t || createCanvas(w = 400, w);
-    background(0); // Svart bakgrund
-    stroke(255, 96);
-
-    for (t += PI / 240, i = 1e4; i--; ) {
-        a(i, i / 235);
-    }
-}
-}
-
